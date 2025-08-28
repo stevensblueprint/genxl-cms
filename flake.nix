3a5e17100ca00f7371364485f52a91468bbdd553
# <me@rebound.sh>
2025-08-28 11:37:31 -0700

    chore: flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = import nixpkgs {
+				inherit system;
+			}; in {
+				devShell = pkgs.mkShell {
+					packages = with pkgs; [
+						nodejs_24
+					];
+				};
+			}
+		);
+}
