a1b4d3f9594afbcad7c45f45333b9e9460eee45e
zd <zidanni.clerigo@gmail.com>
2025-09-22 15:54:57 -0400

    feat/gallery-block

---
+++ src/blocks/Gallery/config.ts
@@ -0,0 +1,34 @@
+import type { Block } from 'payload'
+
+export const Gallery: Block = {
+  slug: 'gallery',
+  interfaceName: 'Gallery',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      label: 'Gallery Title',
+      defaultValue: 'Our Gallery',
+    },
+    {
+      name: 'images',
+      type: 'array',
+      label: 'Gallery Images',
+      minRows: 1,
+      fields: [
+        {
+          name: 'media',
+          type: 'upload',
+          relationTo: 'media',
+          required: true,
+          label: 'Image or Video',
+        },
+        {
+          name: 'caption',
+          type: 'text',
+          label: 'Image Caption',
+        },
+      ],
+    },
+  ],
+}
