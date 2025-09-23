a9311fdbc2f3ac602afd2c09b59783fa318f47a9
Ejinks <ejinks2@stevens.edu>
2025-09-22 20:38:32 -0400

    Created Carousel component.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -13,6 +13,7 @@ import { Posts } from './collections/Posts'
 import { Users } from './collections/Users'
 import { Footer } from './Footer/config'
 import { Header } from './Header/config'
+import { Carousel } from './Carousel/config'
 import { plugins } from './plugins'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
@@ -64,7 +65,7 @@ export default buildConfig({
   }),
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
-  globals: [Header, Footer],
+  globals: [Header, Footer, Carousel],
   plugins: [
     ...plugins,
     // storage-adapter-placeholder
